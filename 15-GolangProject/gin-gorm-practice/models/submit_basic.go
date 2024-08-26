019ceb369f57b7134ab367a142260bc31a7d2eb0
Sratyk Cota <yuuanzuki@gmail.com>
2024-08-26 17:55:53 +0800

    [fix]修正sql语句

--- 15-GolangProject/gin-gorm-practice/models/submit_basic.go
+++ 15-GolangProject/gin-gorm-practice/models/submit_basic.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type SubmitBasic struct {
 	gorm.Model
-	Identity        string `gorm:"column(identity);type:varchar(36);" json:"identity"`                 // 唯一标识
+	Identity        string `gorm:"column(identity);type:varchar(36);" json:"identities"`               // 唯一标识
 	ProblemIdentity string `gorm:"column(problem_identity);type:varchar(36);" json:"problem_identity"` // 问题唯一标识
 	UserIdentity    string `gorm:"column(user_identity);type:varchar(36);" json:"user_identity"`       // 用户唯一标识
 	Status          int    `gorm:"column(status);type:tinyint;" json:"status"`                         // 提交状态
